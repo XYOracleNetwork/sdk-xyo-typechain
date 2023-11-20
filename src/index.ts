bd3eabf9be1ff63ec14b07b5de6c753d8e8f14e1
Arie Trouw <arie.trouw@xylabs.com>
2023-11-20 09:35:25 -0800

    deps cleanup - deploy - 3.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+import * as OpenZeppelin from '@xyo-network/open-zeppelin-typechain'
 import * as XYO from '@xyo-network/typechain'
 import * as Uniswap from '@xyo-network/uniswap-typechain'
 import * as World from '@xyo-network/world-typechain'
 
-export { Uniswap, World, XYO }
+export { OpenZeppelin, Uniswap, World, XYO }
