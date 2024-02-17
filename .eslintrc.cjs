8cfe6b4a96a3a199f0b9b4b5379f907112c4b12f
Arie Trouw <arie.trouw@xylabs.com>
2024-02-17 13:52:51 -0800

    lint

---
+++ .eslintrc.cjs
@@ -0,0 +1,73 @@
+const finalConfig = {
+  env: {
+    es2024: true
+  },
+  'extends': ['@xylabs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', project: './tsconfig.json', sourceType: 'module', tsconfigRootDir: null },
+  "root": true,
+  "ignorePatterns": [
+    "packages/world-typechain/src",
+    "packages/uniswap-typechain/src",
+    "packages/typechain/src",
+    "packages/open-zeppelin-typechain/src",
+    "dist",
+    "node_modules",
+    "docs",
+    "coverage",
+    "docker",
+    "nftData",
+    "testData.json",
+    "*.stories.*",
+    "swagger.json",
+    ".yarn",
+    ".*"
+  ],
+  "rules": {
+    "import/no-default-export": ["off"],
+    "no-restricted-imports": [
+      "warn",
+      {
+        "paths": [
+          "@xyo-network/archivist",
+          "@xyo-network/bridge",
+          "@xyo-network/core",
+          "@xyo-network/diviner",
+          "@xyo-network/module",
+          "@xyo-network/modules",
+          "@xyo-network/node",
+          "@xyo-network/sdk",
+          "@xyo-network/plugins",
+          "@xyo-network/protocol",
+          "@xyo-network/sentinel",
+          "@xyo-network/witness",
+          "@xyo-network/core-payload-plugins",
+          "react-player",
+          "filepond",
+          "aos",
+          "react-icons",
+          ".",
+          "..",
+          "../..",
+          "../../..",
+          "../../../..",
+          "../../../../..",
+          "../../../../../..",
+          "../../../../../../.."
+        ]
+      }
+    ],
+    "import/no-internal-modules": [
+      "warn", {
+        "allow": [
+          "source-map-support/*",
+          "lodash/*",
+          "aws-sdk/**/*",
+          "types/*"
+        ]
+      }
+    ]
+  }
+}
+
+module.exports = finalConfig
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import * as OpenZeppelin from '@xyo-network/open-zeppelin-typechain'
-import * as XYO from '@xyo-network/typechain'
-import * as Uniswap from '@xyo-network/uniswap-typechain'
-import * as World from '@xyo-network/world-typechain'
-
-export { OpenZeppelin, Uniswap, World, XYO }
+export * as OpenZeppelin from '@xyo-network/open-zeppelin-typechain'
+export * as XYO from '@xyo-network/typechain'
+export * as Uniswap from '@xyo-network/uniswap-typechain'
+export * as World from '@xyo-network/world-typechain'
