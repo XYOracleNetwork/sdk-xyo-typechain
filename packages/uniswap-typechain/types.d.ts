f3e28443723d7a49e34872ecf9454348e0e8954a
Arie Trouw <arie.trouw@xylabs.com>
2024-08-29 21:17:30 -0700

    deploy 3.2.2

--- packages/uniswap-typechain/types.d.ts
+++ packages/uniswap-typechain/types.d.ts
@@ -1,3 +1,3 @@
 /* eslint-disable import/no-internal-modules */
-export * from './dist/node'
-export * from './dist/node/common'
+export * from './dist/node/index.d.ts'
+export * from './dist/node/common.d.ts'
--- packages/world-typechain/types.d.ts
+++ packages/world-typechain/types.d.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-internal-modules */
-export type * from './dist/node'
-export type * from './dist/node/common'
+export type * from './dist/node/index.d.ts'
+export type * from './dist/node/common.d.ts'
 
 import type {
   AuctionStartedEvent,
