997ad84e727ebe71d9b5767cc0b05f6a8e256c51
Arie Trouw <arie.trouw@xylabs.com>
2024-12-16 18:01:30 -0800

    Fixed type exports

--- packages/typechain/types.d.ts
+++ packages/typechain/types.d.ts
@@ -1,3 +1,3 @@
 /* eslint-disable import-x/no-internal-modules */
-export * from './dist/node'
-export * from './dist/node/common'
+export * from './dist/node/common.d.ts'
+export * from './dist/node/index.d.ts'
