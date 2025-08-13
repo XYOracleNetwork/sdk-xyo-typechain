740e57b332451e5602dd36d8d468362342905adf
Arie Trouw <arie.trouw@xylabs.com>
2025-08-13 12:16:59 -0700

    glob for windows fixed?

--- packages/open-zeppelin-typechain/scripts/generate-sdk.mjs
+++ packages/open-zeppelin-typechain/scripts/generate-sdk.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import glob from 'fast-glob'
+import { glob } from 'glob'
 import { execSync } from 'node:child_process'
 import { createRequire } from 'node:module'
 import path from 'node:path'
@@ -15,10 +15,11 @@ const require = createRequire(import.meta.url)
 // Resolve the path to the solidity package
 const solidityPath = path.dirname(require.resolve('@openzeppelin/contracts/package.json'))
 
-const files = glob.sync([
-  `${solidityPath}/build/contracts/**/*.json`,
-  `!${solidityPath}/build/contracts/**/*.dbg.json`,
-])
+// Use Node's native glob (introduced in Node.js v16.13.0)
+const files = await glob(`${solidityPath}/build/contracts/**/*.json`, {
+  ignore: `${solidityPath}/build/contracts/**/*.dbg.json`,
+  windowsPathsNoEscape: true, // Handle Windows paths correctly
+})
 
 execSync(
   `typechain --node16-modules --out-dir=./src --target=ethers-v6 ${files.join(' ')}`,
