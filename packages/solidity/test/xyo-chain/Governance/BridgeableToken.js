c3ae019d81032bda757183068c4bd14f2c4b51fb
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-08-15 13:09:32 -0500

    Ensure non-mintable if not owner

--- packages/solidity/test/xyo-chain/Governance/BridgeableToken.js
+++ packages/solidity/test/xyo-chain/Governance/BridgeableToken.js
@@ -6,10 +6,10 @@ const { expect } = chai
 describe('BridgeableToken', function () {
   describe('owner', function () {
     it('Should initially be set to the deployer', async function () {
-      const { token } = await loadFixture(deployBridgeableToken)
       const [deployer] = await ethers.getSigners()
-      const owner = await token.owner()
+      const { token, owner } = await loadFixture(deployBridgeableToken)
       expect(owner).to.equal(deployer.address)
+      expect(await token.owner()).to.equal(deployer.address)
     })
     it('Can be changed after deployment', async function () {
       const { token } = await loadFixture(deployBridgeableToken)
@@ -17,6 +17,22 @@ describe('BridgeableToken', function () {
       await token.transferOwnership(newOwner.address)
       const owner = await token.owner()
       expect(owner).to.equal(newOwner.address)
+    })
+  })
+  describe('mint', function () {
+    const amount = ethers.parseUnits('1000000', 18)
+    it('Should be callable by owner', async function () {
+      const [_, receiver] = await ethers.getSigners()
+      const { token } = await loadFixture(deployBridgeableToken)
+      await token.mint(receiver.address, amount)
+    })
+    it('Should not be callable by other addresses', async function () {
+      const [_, receiver, minter] = await ethers.getSigners()
+      const { token } = await loadFixture(deployBridgeableToken)
+      const contract = token.connect(minter)
+      await expect(
+        contract.mint(receiver.address, amount),
+      ).to.be.reverted
     })
   })
 })
