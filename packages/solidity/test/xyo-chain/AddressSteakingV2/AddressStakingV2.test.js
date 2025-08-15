90f6bed983c79fbbff8aac0897c7101443b5299d
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-08-15 17:18:20 -0500

    Additional tests

--- packages/solidity/test/xyo-chain/AddressSteakingV2/AddressStakingV2.test.js
+++ packages/solidity/test/xyo-chain/AddressSteakingV2/AddressStakingV2.test.js
@@ -3,16 +3,16 @@ import { deployAddressStakingV2 } from '../helpers/index.js'
 import chai from 'chai'
 const { expect } = chai
 
-describe('AddressStakingV2', function () {
+describe('AddressStakingV2', () => {
   const amount = ethers.parseUnits('1000', 18)
 
-  async function mintAndApprove(token, staker, stakingContract, amount) {
+  const mintAndApprove = async (token, staker, stakingContract, amount) => {
     await token.mint(staker.address, amount)
     await token.connect(staker).approve(await stakingContract.getAddress(), amount)
   }
 
-  describe('addStake', function () {
-    it('should allow a staker to add a stake', async function () {
+  describe('addStake', () => {
+    it('should allow a staker to add a stake', async () => {
       const [staker] = await ethers.getSigners()
       const { staking, token } = await loadFixture(deployAddressStakingV2)
 
@@ -21,7 +21,7 @@ describe('AddressStakingV2', function () {
       await expect(tx).to.emit(staking, 'StakeAdded')
     })
 
-    it('should revert if amount is zero', async function () {
+    it('should revert if amount is zero', async () => {
       const [staker] = await ethers.getSigners()
       const { staking, token } = await loadFixture(deployAddressStakingV2)
 
@@ -30,7 +30,100 @@ describe('AddressStakingV2', function () {
 
       await expect(
         staking.connect(staker).addStake(staker.address, 0),
+      ).to.be.revertedWith('Staking: amount must be greater than 0')
+    })
+  })
+
+  describe('removeStake', () => {
+    it('should allow a staker to remove a stake', async () => {
+      const [staker] = await ethers.getSigners()
+      const { staking, token } = await loadFixture(deployAddressStakingV2)
+
+      await mintAndApprove(token, staker, staking, amount)
+      await staking.connect(staker).addStake(staker.address, amount)
+
+      const tx = await staking.connect(staker).removeStake(0)
+      await expect(tx).to.emit(staking, 'StakeRemoved')
+    })
+
+    it('should revert if the stake is already removed', async () => {
+      const [staker] = await ethers.getSigners()
+      const { staking, token } = await loadFixture(deployAddressStakingV2)
+
+      await mintAndApprove(token, staker, staking, amount)
+      await staking.connect(staker).addStake(staker.address, amount)
+      await staking.connect(staker).removeStake(0)
+
+      await expect(
+        staking.connect(staker).removeStake(0),
+      ).to.be.revertedWith('Staking: not removable')
+    })
+  })
+
+  describe('withdrawStake', () => {
+    it('should revert if not enough blocks have passed', async () => {
+      const [staker] = await ethers.getSigners()
+      const { staking, token } = await loadFixture(deployAddressStakingV2)
+
+      await mintAndApprove(token, staker, staking, amount)
+      await staking.connect(staker).addStake(staker.address, amount)
+      await staking.connect(staker).removeStake(0)
+
+      await expect(
+        staking.connect(staker).withdrawStake(0),
+      ).to.be.revertedWith('Staking: not withdrawable')
+    })
+
+    it('should allow withdrawal after required blocks', async () => {
+      const [staker] = await ethers.getSigners()
+      const {
+        staking, token, minWithdrawalBlocks,
+      } = await loadFixture(deployAddressStakingV2)
+
+      await mintAndApprove(token, staker, staking, amount)
+      await staking.connect(staker).addStake(staker.address, amount)
+      await staking.connect(staker).removeStake(0)
+
+      // Mine required number of blocks
+      for (let i = 0; i < minWithdrawalBlocks; i++) {
+        await ethers.provider.send('evm_mine', [])
+      }
+
+      const tx = await staking.connect(staker).withdrawStake(0)
+      await expect(tx).to.emit(staking, 'StakeWithdrawn')
+    })
+  })
+
+  describe('slashStake', () => {
+    it('should allow owner to slash stake', async () => {
+      const [staker, owner] = await ethers.getSigners()
+      const { staking, token } = await loadFixture(deployAddressStakingV2)
+
+      await mintAndApprove(token, staker, staking, amount)
+      await staking.connect(staker).addStake(staker.address, amount)
+
+      const tx = await staking.connect(owner).slashStake(staker.address, amount / 2n)
+      await expect(tx).to.emit(staking, 'StakeSlashed')
+    })
+
+    it('should revert if called by non-owner', async () => {
+      const [staker, nonOwner] = await ethers.getSigners()
+      const { staking, token } = await loadFixture(deployAddressStakingV2)
+
+      await mintAndApprove(token, staker, staking, amount)
+      await staking.connect(staker).addStake(staker.address, amount)
+
+      await expect(
+        staking.connect(nonOwner).slashStake(staker.address, amount / 2n),
       ).to.be.reverted
+    })
+  })
+
+  describe('stakedAddresses', () => {
+    it('should return 0 (stubbed)', async () => {
+      const { staking } = await loadFixture(deployAddressStakingV2)
+      const result = await staking.stakedAddresses(1000)
+      expect(result).to.equal(0)
     })
   })
 })
