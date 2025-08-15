828643d98e6324fbda5580b0da46734c0b99747e
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-08-15 12:52:04 -0500

    Ownable test

--- packages/solidity/test/xyo-chain/Governance/BridgeableToken.js
+++ packages/solidity/test/xyo-chain/Governance/BridgeableToken.js
@@ -6,10 +6,9 @@ const { expect } = chai
 describe('BridgeableToken', function () {
   describe('owner', function () {
     it('Should be from the correct address', async function () {
-      const { sut, owner } = await loadFixture(deployBridgeableToken)
-      const deploymentTx = await sut.deploymentTransaction()
-      const deployerAddress = deploymentTx?.from
-      expect(deployerAddress).to.equal(owner.address)
+      const { token, owner } = await loadFixture(deployBridgeableToken)
+      const tokenOwner = await token.owner()
+      expect(tokenOwner).to.equal(owner.address)
     })
   })
 })
