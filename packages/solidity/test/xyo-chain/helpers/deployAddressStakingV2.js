0feff66e8e725c3d65e9e921c280c08c743fbe7d
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-08-15 17:58:30 -0500

    Return minWithdrawalBlocks from fixture

--- packages/solidity/test/xyo-chain/helpers/deployAddressStakingV2.js
+++ packages/solidity/test/xyo-chain/helpers/deployAddressStakingV2.js
@@ -5,5 +5,7 @@ export const deployAddressStakingV2 = async (minWithdrawalBlocks = 3) => {
   const Staking = await ethers.getContractFactory('AddressStakingV2')
   const staking = await Staking.deploy(minWithdrawalBlocks, await token.getAddress())
   await staking.waitForDeployment()
-  return { staking, token }
+  return {
+    staking, token, minWithdrawalBlocks,
+  }
 }
