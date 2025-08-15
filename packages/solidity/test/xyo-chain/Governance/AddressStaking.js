f34e360a6916acb927049b3edc0bc6da92443988
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-08-15 12:27:56 -0500

    Use no initial supply for BridgeableToken

--- packages/solidity/test/xyo-chain/Governance/AddressStaking.js
+++ packages/solidity/test/xyo-chain/Governance/AddressStaking.js
@@ -1,8 +1,4 @@
-import { anyValue } from '@nomicfoundation/hardhat-chai-matchers/withArgs.js'
-import {
-  loadFixture,
-  time,
-} from '@nomicfoundation/hardhat-toolbox/network-helpers.js'
+import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers.js'
 import chai from 'chai'
 
 const { expect } = chai
@@ -12,10 +8,10 @@ describe('AddressStaking', function () {
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   async function deployOneYearAddressStakingFixture() {
-    // Deploy a mock ERC20 token to use as staking token
-    const initialSupply = ethers.parseUnits('1000000', 18)
+    // Deploy a BridgeableToken token to use as staking token
+    // const initialSupply = ethers.parseUnits('1000000', 18)
     const TokenFactory = await ethers.getContractFactory('BridgeableToken')
-    const stakingToken = await TokenFactory.deploy('Test Token', 'TEST', initialSupply)
+    const stakingToken = await TokenFactory.deploy('Test Token', 'TEST')
 
     // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await ethers.getSigners()
@@ -24,7 +20,7 @@ describe('AddressStaking', function () {
     const sut = await AddressStaking.deploy(1, stakingToken.target)
 
     return {
-      sut, owner, otherAccount,
+      sut, owner, otherAccount, stakingToken,
     }
   }
 
