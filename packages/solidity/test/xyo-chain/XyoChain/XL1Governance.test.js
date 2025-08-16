111aa35adcfb2dcbc755a6741ccae346f298aeb5
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-08-16 13:12:10 -0500

    Scaffold further tests

--- packages/solidity/test/xyo-chain/XyoChain/XL1Governance.test.js
+++ packages/solidity/test/xyo-chain/XyoChain/XL1Governance.test.js
@@ -1,11 +1,64 @@
 import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers.js'
 import chai from 'chai'
 const { expect } = chai
-import { deployXL1Governance } from '../helpers/index.js'
+import {
+  deployXL1Governance, DefaultVotingDelay, DefaultVotingPeriod,
+} from '../helpers/index.js'
 
 describe('XL1Governance', () => {
-  it('matches calcBlockReward', async () => {
-    const { xl1Governance, deployer } = await loadFixture(deployXL1Governance)
-    expect(true).to.equal(true)
+  it('should return the current block number as clock()', async () => {
+    const { xl1Governance } = await loadFixture(deployXL1Governance)
+    const currentBlock = await ethers.provider.getBlockNumber()
+
+    expect(await xl1Governance.clock()).to.equal(currentBlock)
+  })
+
+  it('should return the correct CLOCK_MODE string', async () => {
+    const { xl1Governance } = await loadFixture(deployXL1Governance)
+    expect(await xl1Governance.CLOCK_MODE()).to.equal('mode=blocknumber&from=default')
+  })
+
+  it('should have a quorum equal to the number of governors', async () => {
+    const { xl1Governance } = await loadFixture(deployXL1Governance)
+    expect(await xl1Governance.quorum(0)).to.equal(await xl1Governance.governorCount())
+  })
+
+  it('should allow checking vote power based on governor membership', async () => {
+    const { xl1Governance } = await loadFixture(deployXL1Governance)
+    const [signerA] = await ethers.getSigners()
+
+    const fakeGovernor = await (
+      await ethers.getContractFactory('SingleAddressSubGovernor')
+    ).deploy(xl1Governance)
+
+    // Initially not a governor, should return 0
+    expect(await xl1Governance.getVotes(await fakeGovernor.getAddress(), 0)).to.equal(0)
+
+    // Add governor and expect vote weight to be 1
+    await xl1Governance.addGovernor(fakeGovernor)
+    expect(await xl1Governance.getVotes(await fakeGovernor.getAddress(), 0)).to.equal(1)
+  })
+
+  it('should return true for known interfaces', async () => {
+    const { xl1Governance } = await loadFixture(deployXL1Governance)
+
+    // Use a known interface from OpenZeppelin Governor
+    const INTERFACE_ID_GOVERNOR = '0x4f851a7e' // IGovernor interface ID
+    expect(await xl1Governance.supportsInterface(INTERFACE_ID_GOVERNOR)).to.equal(true)
+  })
+
+  describe('votingDelay', () => {
+    it('should return voting delay', async () => {
+      const { xl1Governance } = await loadFixture(deployXL1Governance)
+
+      expect(await xl1Governance.votingDelay()).to.equal(DefaultVotingDelay)
+    })
+  })
+  describe('votingPeriod', () => {
+    it('should return voting period', async () => {
+      const { xl1Governance } = await loadFixture(deployXL1Governance)
+
+      expect(await xl1Governance.votingPeriod()).to.equal(DefaultVotingPeriod)
+    })
   })
 })
