f98449a96ecb47e34f0afed35af2eb29c39ed716
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-08-16 14:29:57 -0500

    Remove mock governor

--- packages/solidity/test/xyo-chain/helpers/deploySingleAddressSubGovernor.js
+++ packages/solidity/test/xyo-chain/helpers/deploySingleAddressSubGovernor.js
@@ -1,12 +1,5 @@
-export const deployMockGovernor = async () => {
-  const MockGovernor = await ethers.getContractFactory('MockGovernor') // TODO: Create this or use an existing implementation
-  const mockGovernor = await MockGovernor.deploy()
-  return { mockGovernor }
-}
-
 export const deploySingleAddressSubGovernor = async () => {
   const [deployer] = await ethers.getSigners()
-  const { mockGovernor } = await deployMockGovernor()
 
   const SubGovFactory = await ethers.getContractFactory('SingleAddressSubGovernor')
   const subGovernor = await SubGovFactory.deploy(await mockGovernor.getAddress())
