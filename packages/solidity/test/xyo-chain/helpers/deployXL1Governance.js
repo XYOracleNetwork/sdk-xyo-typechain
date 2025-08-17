4c576738ffe18f27c237c2f515459337963c3903
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-08-16 21:04:01 -0500

    update helper to match new ctor

--- packages/solidity/test/xyo-chain/helpers/deployXL1Governance.js
+++ packages/solidity/test/xyo-chain/helpers/deployXL1Governance.js
@@ -1,11 +1,17 @@
+export const XL1GovernanceDefaultName = 'XL1Governance'
 export const XL1GovernanceDefaultVotingDelay = 1
 export const XL1GovernanceDefaultVotingPeriod = 5
 
-export const deployXL1Governance = async (votingDelay = XL1GovernanceDefaultVotingDelay, votingPeriod = XL1GovernanceDefaultVotingPeriod) => {
+export const deployXL1Governance = async (
+  governors,
+  name = XL1GovernanceDefaultName,
+  votingDelay = XL1GovernanceDefaultVotingDelay,
+  votingPeriod = XL1GovernanceDefaultVotingPeriod,
+) => {
   const [deployer] = await ethers.getSigners()
 
   const XL1Governance = await ethers.getContractFactory('XL1Governance')
-  const xl1Governance = await XL1Governance.deploy(votingDelay, votingPeriod)
+  const xl1Governance = await XL1Governance.deploy(name, governors, votingDelay, votingPeriod)
 
   return { xl1Governance, deployer }
 }
