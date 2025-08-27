f8faa1f5689f72f36fd535110391a3c9acd7ec7a
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-08-27 15:46:37 -0500

    Update defaults

--- packages/solidity/ignition/modules/BridgeableToken.js
+++ packages/solidity/ignition/modules/BridgeableToken.js
@@ -1,14 +1,11 @@
-// This setup uses Hardhat Ignition to manage smart contract deployments.
-// Learn more about it at https://hardhat.org/ignition
-
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
 
-const TOKEN_NAME = 'Token Name'
-const TOKEN_SYMBOL = 'TOKN'
+const DEFAULT_TOKEN_NAME = 'Token Name'
+const DEFAULT_TOKEN_SYMBOL = 'TOKN'
 
 export default buildModule('BridgeableToken', (m) => {
-  const name = m.getParameter('unlockTime', TOKEN_NAME)
-  const symbol = m.getParameter('lockedAmount', TOKEN_SYMBOL)
+  const name = m.getParameter('unlockTime', DEFAULT_TOKEN_NAME)
+  const symbol = m.getParameter('lockedAmount', DEFAULT_TOKEN_SYMBOL)
 
   const token = m.contract('BridgeableToken', [name, symbol])
 
