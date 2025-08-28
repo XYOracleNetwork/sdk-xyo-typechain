6c948a1cd918079659a2a949e50ccdbdb957f47c
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-08-28 13:38:57 -0500

    Deploy SingleAddressSubGovernorModule as E2E

--- packages/solidity/ignition/modules/DeployXL1.ts
+++ packages/solidity/ignition/modules/DeployXL1.ts
@@ -1,10 +1,7 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
+import { SingleAddressSubGovernorModule } from './SingleAddressSubGovernor'
 
 export default buildModule('DeployXL1', (m) => {
-  const name = m.getParameter('unlockTime', TOKEN_NAME)
-  const symbol = m.getParameter('lockedAmount', TOKEN_SYMBOL)
-
-  const token = m.contract('BridgeableToken', [name, symbol])
-
-  return { token }
+  const { subGovernor } = m.useModule(SingleAddressSubGovernorModule("Name", 1, 1))
+  return { subGovernor }
 })
