474c618b163ec490f9af485c8ed95bb5fad15008
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-08-28 14:09:19 -0500

    Multiple exports

--- packages/solidity/ignition/modules/BridgeableToken.ts
+++ packages/solidity/ignition/modules/BridgeableToken.ts
@@ -1,22 +1,17 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
+import type { IgnitionModuleBuilder } from '@nomicfoundation/ignition-core'
 
 const TOKEN_NAME = 'Token Name'
 const TOKEN_SYMBOL = 'TOKN'
 
-export const BridgeableTokenModule = buildModule('BridgeableToken', (m) => {
-  const name = m.getParameter('unlockTime', TOKEN_NAME)
-  const symbol = m.getParameter('lockedAmount', TOKEN_SYMBOL)
+const createBridgeableTokenModule = (m: IgnitionModuleBuilder) => {
+  const name = m.getParameter('tokenName', TOKEN_NAME)
+  const symbol = m.getParameter('tokenSymbol', TOKEN_SYMBOL)
 
   const token = m.contract('BridgeableToken', [name, symbol])
-
   return { token }
-})
+}
 
-export default buildModule('BridgeableToken', (m) => {
-  const name = m.getParameter('unlockTime', TOKEN_NAME)
-  const symbol = m.getParameter('lockedAmount', TOKEN_SYMBOL)
+export const BridgeableTokenModule = buildModule('BridgeableToken', createBridgeableTokenModule)
 
-  const token = m.contract('BridgeableToken', [name, symbol])
-
-  return { token }
-})
+export default BridgeableTokenModule
