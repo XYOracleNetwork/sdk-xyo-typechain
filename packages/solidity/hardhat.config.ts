1fabf91a34443932ee4eb73e8da621c989c1c53f
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-08-28 10:59:25 -0500

    Add testing tools

--- packages/solidity/hardhat.config.ts
+++ packages/solidity/hardhat.config.ts
@@ -1,7 +1,10 @@
 /* eslint-disable import-x/no-internal-modules */
 import type { HardhatUserConfig } from 'hardhat/config'
 
+import hardhatToolboxMochaEthers from "@nomicfoundation/hardhat-toolbox-mocha-ethers"
+
 const config: HardhatUserConfig = {
+  plugins: [hardhatToolboxMochaEthers],
   solidity: {
     version: '0.8.24',
     settings: {
