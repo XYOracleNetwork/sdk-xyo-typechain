681dd5f140579ff309f35f624ca3c8ad4e76e7e6
Arie Trouw <arie.trouw@xylabs.com>
2025-08-28 11:19:06 -0700

    Re-exporting types from solidity in typechain

--- packages/typechain/types.d.ts
+++
@@ -1,3 +0,0 @@
-/* eslint-disable import-x/no-internal-modules */
-export * from './dist/neutral/common.d.ts'
-export * from './dist/neutral/index.d.ts'
--- packages/typechain/xy.config.ts
+++ packages/typechain/xy.config.ts
@@ -1,7 +1,6 @@
 import type { XyTsupConfig } from '@xylabs/ts-scripts-yarn3'
 const config: XyTsupConfig = {
   compile: {
-    entryMode: 'all',
     browser: {},
     neutral: { src: true },
     node: {},
