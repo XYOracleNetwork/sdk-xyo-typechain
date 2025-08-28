58f93b7fb386e4e9ac995bc7f1a1b0ceaa248e35
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-08-28 13:44:29 -0500

    Deploy full contract suite

--- packages/solidity/ignition/modules/DeployXL1.ts
+++ packages/solidity/ignition/modules/DeployXL1.ts
@@ -1,5 +1,19 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
-import { SingleAddressSubGovernorModule } from './SingleAddressSubGovernor'
+
+export const SingleAddressSubGovernorModule = (
+  name: string,
+  votingDelay: number,
+  votingPeriod: number
+) =>
+  buildModule("SingleAddressSubGovernor", (m) => {
+    const subGovernor = m.contract("SingleAddressSubGovernor", [
+      name,
+      votingDelay,
+      votingPeriod,
+    ]);
+
+    return { subGovernor };
+  });
 
 export default buildModule('DeployXL1', (m) => {
   const { subGovernor } = m.useModule(SingleAddressSubGovernorModule("Name", 1, 1))
