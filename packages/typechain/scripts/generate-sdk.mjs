1858575243772cbfe662d3ad982e4314c465c955
Arie Trouw <arie.trouw@xylabs.com>
2025-08-28 12:39:08 -0700

    typechain package fixed

--- packages/typechain/scripts/generate-sdk.mjs
+++ packages/typechain/scripts/generate-sdk.mjs
@@ -1,23 +1,15 @@
 #!/usr/bin/env node
 
 import { execSync } from 'node:child_process'
-import { glob } from 'glob'
+import { glob, globSync } from 'glob'
 import fs from 'node:fs'
-import { createRequire } from 'node:module'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 
-// Get the current directory
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
-// Create a require function
-const require = createRequire(import.meta.url)
-
-// Resolve the path to the solidity package
-const solidityPath = path.dirname(require.resolve('@xyo-network/solidity/package.json'))
+export function findFilesByGlob(cwd, pattern) {
+  return globSync(pattern, { cwd, absolute: true })
+}
 
 // First, delete any dbg.json files
-const dbgFiles = await glob(`${solidityPath}/artifacts/contracts/**/*.dbg.json`)
+const dbgFiles = await glob('artifacts/contracts/**/*.dbg.json')
 for (const file of dbgFiles) {
   try {
     fs.unlinkSync(file)
@@ -26,9 +18,10 @@ for (const file of dbgFiles) {
   }
 }
 
-const includeFiles = `${solidityPath}/artifacts/contracts/**/*.json`
+const files = findFilesByGlob(process.cwd(), 'artifacts/contracts/**/*.json')
+const filesToProcess = files.filter(file => !file.endsWith('.dbg.json'))
 
 execSync(
-  `typechain --node16-modules --out-dir=./src --target=ethers-v6 ${includeFiles}`,
+  `typechain --node16-modules --out-dir=./src --target=ethers-v6 ${filesToProcess.join(' ')}`,
   { stdio: 'inherit' },
 )
