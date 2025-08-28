52b1cd2e775e9818472e22fb0dc040fedb55aee4
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-08-28 10:48:52 -0500

    Working solidity compile

--- packages/solidity/hardhat.config.ts
+++ packages/solidity/hardhat.config.ts
@@ -1,6 +1,18 @@
 /* eslint-disable import-x/no-internal-modules */
 import type { HardhatUserConfig } from 'hardhat/config'
 
-const config: HardhatUserConfig = {}
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.8.24',
+    settings: {
+      viaIR: true,
+      evmVersion: 'cancun',
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+}
 
 export default config
