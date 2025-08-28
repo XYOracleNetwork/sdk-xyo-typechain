951648244621fd1eaf5be07332ffbfb39be2cafa
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-08-28 14:40:46 -0500

    XyoChainV2 deploy

--- packages/solidity/ignition/modules/DeployXL1.ts
+++ packages/solidity/ignition/modules/DeployXL1.ts
@@ -2,12 +2,16 @@ import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
 
 import { BridgeableTokenModule } from './BridgeableToken'
 import { SingleAddressSubGovernorModule } from './SingleAddressSubGovernor'
+import { createStakedXyoChainV2Module } from './StakedXyoChainV2'
 import { createXL1GovernanceModule } from './XL1Governance'
+import { createXyoChainRewardsModule } from './XyoChainRewards'
 
 export default buildModule('DeployXL1', (m) => {
   const { token } = m.useModule(BridgeableTokenModule)
   const { subGovernor } = m.useModule(SingleAddressSubGovernorModule)
   const { xl1Governance } = m.useModule(createXL1GovernanceModule(subGovernor))
+  const { rewards } = createXyoChainRewardsModule(m)
+  const { chain } = createStakedXyoChainV2Module(token, rewards)(m)
   return {
     subGovernor, token, xl1Governance,
   }
