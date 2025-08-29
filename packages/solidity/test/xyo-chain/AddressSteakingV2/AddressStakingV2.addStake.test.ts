225448a273ca1c50fa86e22a65ce4fbe2256bf58
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-08-29 11:57:37 -0500

    Multi-staker tests

--- packages/solidity/test/xyo-chain/AddressSteakingV2/AddressStakingV2.addStake.test.ts
+++ packages/solidity/test/xyo-chain/AddressSteakingV2/AddressStakingV2.addStake.test.ts
@@ -8,7 +8,7 @@ import { advanceBlocks, deployAddressStakingV2 } from '../helpers/index.js'
 
 const { ethers } = hre
 
-describe('AddressStakingV2.addStake', () => {
+describe.only('AddressStakingV2.addStake', () => {
   const amount = ethers.parseUnits('1000', 18)
 
   const mintAndApprove = async (token: BridgeableToken, staker: HardhatEthersSigner, stakingContract: AddressStakingV2, amount: bigint) => {
@@ -16,26 +16,37 @@ describe('AddressStakingV2.addStake', () => {
     await token.connect(staker).approve(await stakingContract.getAddress(), amount)
   }
 
-  describe('addStake', () => {
-    it('should allow a staker to add a stake', async () => {
-      const [_, staker] = await ethers.getSigners()
+  it('should allow a staker to add a stake', async () => {
+    const [_, staker] = await ethers.getSigners()
+    const { staking, token } = await loadFixture(deployAddressStakingV2)
+
+    await mintAndApprove(token, staker, staking, amount)
+    const tx = await staking.connect(staker).addStake(staker, amount)
+    await expect(tx).to.emit(staking, 'StakeAdded')
+  })
+
+  it('should revert if amount is zero', async () => {
+    const [staker] = await ethers.getSigners()
+    const { staking, token } = await loadFixture(deployAddressStakingV2)
+
+    await token.mint(staker, amount)
+    await token.connect(staker).approve(await staking.getAddress(), amount)
+
+    await expect(
+      staking.connect(staker).addStake(staker, 0),
+    ).to.be.revertedWith('Staking: amount must be greater than 0')
+  })
+  describe('with multiple stakers', () => {
+    it('should allow multiple stakers to add a stake', async () => {
+      const [_, stakerA, stakerB] = await ethers.getSigners()
       const { staking, token } = await loadFixture(deployAddressStakingV2)
+      const stakers = [stakerA, stakerB]
 
-      await mintAndApprove(token, staker, staking, amount)
-      const tx = await staking.connect(staker).addStake(staker, amount)
-      await expect(tx).to.emit(staking, 'StakeAdded')
-    })
-
-    it('should revert if amount is zero', async () => {
-      const [staker] = await ethers.getSigners()
-      const { staking, token } = await loadFixture(deployAddressStakingV2)
-
-      await token.mint(staker, amount)
-      await token.connect(staker).approve(await staking.getAddress(), amount)
-
-      await expect(
-        staking.connect(staker).addStake(staker, 0),
-      ).to.be.revertedWith('Staking: amount must be greater than 0')
+      for (const staker of stakers) {
+        await mintAndApprove(token, staker, staking, amount)
+        const tx = await staking.connect(staker).addStake(staker, amount)
+        await expect(tx).to.emit(staking, 'StakeAdded')
+      }
     })
   })
 })
