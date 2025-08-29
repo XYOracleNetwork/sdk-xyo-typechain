dad23940175c92e74888845cf02a4db883b72e66
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-08-29 12:37:33 -0500

    Deeper state assertions

--- packages/solidity/test/xyo-chain/AddressSteakingV2/AddressStakingV2.addStake.test.ts
+++ packages/solidity/test/xyo-chain/AddressSteakingV2/AddressStakingV2.addStake.test.ts
@@ -42,12 +42,15 @@ describe('AddressStakingV2.addStake', () => {
         const [_, staked, stakerA, stakerB, stakerC] = await ethers.getSigners()
         const { staking, token } = await loadFixture(deployAddressStakingV2)
         const stakers = [stakerA, stakerB, stakerC]
-
+        let totalStaked: bigint = 0n
         for (const staker of stakers) {
+          totalStaked += amount
           await mintAndApprove(token, staker, staking, amount)
           const tx = await staking.connect(staker).addStake(staked, amount)
           await expect(tx).to.emit(staking, 'StakeAdded')
+          expect(await staking.activeByStaker(staker)).to.equal(amount)
         }
+        expect(await staking.activeByAddressStaked(staked)).to.equal(totalStaked)
       })
     })
     describe('more than the max number of stakers', () => {
@@ -55,13 +58,18 @@ describe('AddressStakingV2.addStake', () => {
         const [_, staked, stakerA, stakerB, stakerC, stakerD] = await ethers.getSigners()
         const { staking, token } = await loadFixture(deployAddressStakingV2)
         const stakers = [stakerA, stakerB, stakerC, stakerD]
+        const resultantStakers = new Set([stakerB, stakerC, stakerD])
+        let resultantStake: bigint = 0n
 
         for (const [i, staker] of stakers.entries()) {
           const stakeAmount = amount * (BigInt(i) + 1n)
+          if (resultantStakers.has(staker)) resultantStake += stakeAmount
           await mintAndApprove(token, staker, staking, stakeAmount)
           const tx = await staking.connect(staker).addStake(staked, stakeAmount)
           await expect(tx).to.emit(staking, 'StakeAdded')
+          expect(await staking.activeByStaker(staker)).to.equal(amount)
         }
+        expect(await staking.activeByAddressStaked(staked)).to.equal(resultantStake)
       })
     })
   })
