dd2305923faf8deb3d12ae40a23fb1bba9c98ce8
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-09-02 13:45:26 -0500

    Governor removal test

--- packages/solidity/test/xyo-chain/Governance/GovernorGroup.test.ts
+++ packages/solidity/test/xyo-chain/Governance/GovernorGroup.test.ts
@@ -2,10 +2,11 @@ import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers.js
 import { expect } from 'chai'
 import hre from 'hardhat'
 
+import type { SingleAddressSubGovernor } from '../../../typechain-types/index.js'
 import {
-  advanceBlocks,
-  deploySingleAddressSubGovernor, deployXL1GovernanceWithSingleAddressSubGovernor, ProposalState, ProposalVote, proposeToCallSmartContract,
-  voteThroughSubGovernor,
+  advanceBlocks, deploySingleAddressSubGovernor, deployXL1Governance, deployXL1GovernanceWithSingleAddressSubGovernor,
+  proposeToCallSmartContract, voteThroughSubGovernor,
+  voteThroughSubGovernors,
 } from '../helpers/index.js'
 
 const { ethers } = hre
@@ -48,5 +49,53 @@ describe('GovernorGroup', () => {
     // Ensure subGovernor is governor so they can vote on proposals
     expect(await xl1Governance.governorCount()).to.equal(2)
     expect(await xl1Governance.isGovernor(await newSubGovernor.getAddress())).to.equal(true)
+  })
+  it('should allow removing governors', async () => {
+    const [_, proposer] = await ethers.getSigners()
+
+    const governorCount = 3
+    const governors: SingleAddressSubGovernor[] = []
+
+    for (let i = 0; i < governorCount; i++) {
+      const fixture = () => deploySingleAddressSubGovernor(`subGovernor${i}`)
+      const { subGovernor: newSubGovernor } = await loadFixture(fixture)
+      governors.push(newSubGovernor)
+    }
+    const fixture = () => deployXL1Governance(governors)
+    const { xl1Governance } = await loadFixture(fixture)
+
+    // Ensure subGovernors exist
+    expect(await xl1Governance.governorCount()).to.equal(governorCount)
+    for (const governor of governors) {
+      expect(await xl1Governance.isGovernor(await governor.getAddress())).to.equal(true)
+    }
+
+    const governorToRemove = governors.at(-1)!
+    const governorToRemoveAddress = await governorToRemove?.getAddress()
+
+    // Create proposal to add new subGovernor
+    const {
+      proposalId: parentProposalId, targets, values, calldatas, descriptionHash,
+    } = await proposeToCallSmartContract(xl1Governance, 'removeGovernor', [governorToRemoveAddress], xl1Governance, proposer)
+
+    // Cast vote via subGovernor
+    await voteThroughSubGovernors({
+      parentGovernor: xl1Governance, subGovernors: governors, parentProposalId, proposer, voteType: 'For',
+    })
+
+    // Move past voting period
+    await advanceBlocks(await governorToRemove.votingPeriod() + 10n)
+
+    // Verify subGovernor has voted on parent proposal
+    for (const subGovernor of governors) {
+      expect(await xl1Governance.hasVoted(parentProposalId, await subGovernor.getAddress())).to.equal(true)
+    }
+
+    // Execute the parent proposal
+    await xl1Governance.execute(targets, values, calldatas, descriptionHash)
+
+    // Ensure subGovernor is governor so they can vote on proposals
+    expect(await xl1Governance.governorCount()).to.equal(governors.length - 1)
+    expect(await xl1Governance.isGovernor(await governorToRemove.getAddress())).to.equal(false)
   })
 })
