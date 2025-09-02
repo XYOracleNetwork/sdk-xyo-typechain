88353a6fba05d7b014d61b33b628a139d044c0f3
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-09-02 07:18:21 -0500

    Check deploy scripts

--- packages/solidity/scripts/CheckDeployment.ts
+++ packages/solidity/scripts/CheckDeployment.ts
@@ -11,9 +11,16 @@ async function main() {
   const deploymentFile = path.join(__dirname, `../ignition/deployments/chain-${chainId}/deployed_addresses.json`)
   const deployments = JSON.parse(readFileSync(deploymentFile, 'utf8'))
 
-  const contract = await hre.ethers.getContractAt('SingleAddressSubGovernor', deployments['SingleAddressSubGovernor#SingleAddressSubGovernor'])
-  const result = await contract.owner()
-  console.log('Result:', result)
+  const [signer] = await hre.ethers.getSigners()
+  const token = await hre.ethers.getContractAt('BridgeableToken', deployments['BridgeableToken#BridgeableToken'], signer)
+  const tokenOwner = await token.owner()
+  console.log('Token Owner Address:', tokenOwner)
+  const address = signer.address
+  console.log('Signer Address:', address)
+  await token.mint(signer.address, 1000n)
+  const balance = await token.balanceOf(signer.address)
+  console.log('Token Balance:', balance.toString())
+  // const subGovernor = await hre.ethers.getContractAt('SingleAddressSubGovernor', deployments['SingleAddressSubGovernor#SingleAddressSubGovernor'])
 }
 
 main().catch(console.error)
