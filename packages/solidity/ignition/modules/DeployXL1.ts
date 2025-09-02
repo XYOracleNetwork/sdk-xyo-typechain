d5985ea991d06da2e7d9ca41709b69148dfb24bb
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-09-02 15:20:52 -0500

    Pass in staking token address via ENV VAR

--- packages/solidity/ignition/modules/DeployXL1.ts
+++ packages/solidity/ignition/modules/DeployXL1.ts
@@ -11,7 +11,9 @@ export default buildModule('DeployXL1', (m) => {
   const { subGovernor } = m.useModule(SingleAddressSubGovernorModule)
   const { xl1Governance } = m.useModule(createXL1GovernanceModule(subGovernor))
   const { rewards } = createXyoChainRewardsModule(m)
-  const { chain } = createStakedXyoChainV2Module(rewards, token)(m)
+  const stakingTokenAddress = process.env.STAKING_TOKEN_ADDRESS ?? ''
+  const { chain } = createStakedXyoChainV2Module(rewards, stakingTokenAddress)(m)
+  m.call(token, 'transferOwnership', [xl1Governance])
   return {
     chain, subGovernor, token, xl1Governance,
   }
