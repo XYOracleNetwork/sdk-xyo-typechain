ddf253a20fdaf2d4645d9529a6f18a92f7933b56
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-09-02 19:40:32 -0500

    XL1Governance params from config

--- packages/solidity/ignition/modules/XL1Governance.ts
+++ packages/solidity/ignition/modules/XL1Governance.ts
@@ -2,17 +2,13 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
 import type { ContractFuture, IgnitionModuleBuilder } from '@nomicfoundation/ignition-core'
 
-const DEFAULT_NAME = 'XL1Governance'
-const DEFAULT_VOTING_DELAY = 1
-const DEFAULT_VOTING_PERIOD = 20
-
 export const createXL1GovernanceModule = (
   subGovernor: ContractFuture<'SingleAddressSubGovernor'>,
 ) => {
   return buildModule('XL1Governance', (m: IgnitionModuleBuilder) => {
-    const name = m.getParameter('name', DEFAULT_NAME)
-    const votingDelay = m.getParameter('votingDelay', DEFAULT_VOTING_DELAY)
-    const votingPeriod = m.getParameter('votingPeriod', DEFAULT_VOTING_PERIOD)
+    const name = m.getParameter('name')
+    const votingDelay = m.getParameter('votingDelay')
+    const votingPeriod = m.getParameter('votingPeriod')
 
     const xl1Governance = m.contract('XL1Governance', [
       name,
