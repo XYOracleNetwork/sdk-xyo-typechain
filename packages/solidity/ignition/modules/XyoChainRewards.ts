7406696387b8e65e721cea6744bdc519749a3ace
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-09-02 19:18:04 -0500

    Pull from config for deploying rewards

--- packages/solidity/ignition/modules/XyoChainRewards.ts
+++ packages/solidity/ignition/modules/XyoChainRewards.ts
@@ -1,16 +1,14 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
 import type { IgnitionModuleBuilder } from '@nomicfoundation/ignition-core'
 
-import { DEFAULT_XYO_REWARD_CONFIG } from './ContractDefaults'
-
 export const createXyoChainRewardsModule = (m: IgnitionModuleBuilder) => {
-  const initialReward = m.getParameter('initialReward', DEFAULT_XYO_REWARD_CONFIG.initialReward)
-  const stepSize = m.getParameter('stepSize', DEFAULT_XYO_REWARD_CONFIG.stepSize)
-  const stepFactorNumerator = m.getParameter('stepFactorNumerator', DEFAULT_XYO_REWARD_CONFIG.stepFactorNumerator)
-  const stepFactorDenominator = m.getParameter('stepFactorDenominator', DEFAULT_XYO_REWARD_CONFIG.stepFactorDenominator)
-  const minRewardPerBlock = m.getParameter('minRewardPerBlock', DEFAULT_XYO_REWARD_CONFIG.minRewardPerBlock)
-  const genesisReward = m.getParameter('genesisReward', DEFAULT_XYO_REWARD_CONFIG.genesisReward)
-  const floorPlaces = m.getParameter('floorPlaces', DEFAULT_XYO_REWARD_CONFIG.floorPlaces)
+  const initialReward = m.getParameter('initialReward')
+  const stepSize = m.getParameter('stepSize')
+  const stepFactorNumerator = m.getParameter('stepFactorNumerator')
+  const stepFactorDenominator = m.getParameter('stepFactorDenominator')
+  const minRewardPerBlock = m.getParameter('minRewardPerBlock')
+  const genesisReward = m.getParameter('genesisReward')
+  const floorPlaces = m.getParameter('floorPlaces')
 
   const rewards = m.contract('XyoChainRewards', [
     initialReward,
