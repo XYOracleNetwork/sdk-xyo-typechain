b02b74dba6bf660df0d342d0029ef667679b354f
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-09-02 19:36:19 -0500

    Formatting of XyoChainRewardsModule

--- packages/solidity/ignition/modules/DeployXL1.ts
+++ packages/solidity/ignition/modules/DeployXL1.ts
@@ -4,13 +4,13 @@ import { BridgeableTokenModule } from './BridgeableToken'
 import { SingleAddressSubGovernorModule } from './SingleAddressSubGovernor'
 import { createStakedXyoChainV2Module } from './StakedXyoChainV2'
 import { createXL1GovernanceModule } from './XL1Governance'
-import { createXyoChainRewardsModule } from './XyoChainRewards'
+import { XyoChainRewardsModule } from './XyoChainRewards'
 
 export default buildModule('DeployXL1', (m) => {
   const { token } = m.useModule(BridgeableTokenModule)
   const { subGovernor } = m.useModule(SingleAddressSubGovernorModule)
   const { xl1Governance } = m.useModule(createXL1GovernanceModule(subGovernor))
-  const { rewards } = createXyoChainRewardsModule(m)
+  const { rewards } = m.useModule(XyoChainRewardsModule)
   const stakingTokenAddress = process.env.STAKING_TOKEN_ADDRESS ?? ''
   const { chain } = createStakedXyoChainV2Module(rewards, stakingTokenAddress)(m)
   m.call(token, 'transferOwnership', [xl1Governance])
--- packages/solidity/ignition/modules/XyoChainRewards.ts
+++ packages/solidity/ignition/modules/XyoChainRewards.ts
@@ -2,13 +2,13 @@ import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
 import type { IgnitionModuleBuilder } from '@nomicfoundation/ignition-core'
 
 export const createXyoChainRewardsModule = (m: IgnitionModuleBuilder) => {
-  const initialReward = m.getParameter('initialReward')
-  const stepSize = m.getParameter('stepSize')
-  const stepFactorNumerator = m.getParameter('stepFactorNumerator')
-  const stepFactorDenominator = m.getParameter('stepFactorDenominator')
-  const minRewardPerBlock = m.getParameter('minRewardPerBlock')
-  const genesisReward = m.getParameter('genesisReward')
   const floorPlaces = m.getParameter('floorPlaces')
+  const genesisReward = m.getParameter('genesisReward')
+  const initialReward = m.getParameter('initialReward')
+  const minRewardPerBlock = m.getParameter('minRewardPerBlock')
+  const stepFactorDenominator = m.getParameter('stepFactorDenominator')
+  const stepFactorNumerator = m.getParameter('stepFactorNumerator')
+  const stepSize = m.getParameter('stepSize')
 
   const rewards = m.contract('XyoChainRewards', [
     initialReward,
