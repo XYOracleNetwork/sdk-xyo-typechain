7fe1f3bee0e74cc77db269047d6e3c651158c38b
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-09-04 11:07:00 -0500

    stakedAddressWithMinStake removal tests

--- packages/solidity/test/xyo-chain/AddressSteakingV2/AddressStakingV2.stakedAddressWithMinStake.ts
+++ packages/solidity/test/xyo-chain/AddressSteakingV2/AddressStakingV2.stakedAddressWithMinStake.ts
@@ -6,7 +6,7 @@ import { deployAddressStakingV2, mintAndApprove } from '../helpers/index.js'
 
 const { ethers } = hre
 
-describe.only('AddressStakingV2.stakedAddressWithMinStake', () => {
+describe('AddressStakingV2.stakedAddressWithMinStake', () => {
   describe('with single staker', () => {
     it('should show address when address has enough staked', async () => {
       // Arrange
@@ -71,6 +71,44 @@ describe.only('AddressStakingV2.stakedAddressWithMinStake', () => {
         await mintAndApprove(token, staker, staking, amount)
         await staking.connect(staker).addStake(staked, amount)
       }
+
+      // Act
+      const addresses = await staking.stakedAddressesWithMinStake()
+
+      // Assert
+      expect(addresses.length).to.equal(0)
+    })
+  })
+  describe('with removed stake', () => {
+    it('should show address when address has enough staked', async () => {
+      // Arrange
+      const [_, staked, staker] = await ethers.getSigners()
+      const { staking, token } = await loadFixture(deployAddressStakingV2)
+      const minStake = await staking.minStake()
+      const amount = minStake * 2n
+      await mintAndApprove(token, staker, staking, amount)
+      await staking.connect(staker).addStake(staked, amount / 2n)
+      await staking.connect(staker).addStake(staked, amount / 2n)
+      await staking.connect(staker).removeStake(0)
+
+      // Act
+      const addresses = await staking.stakedAddressesWithMinStake()
+
+      // Assert
+      expect(addresses.length).to.equal(1)
+      expect(addresses[0]).to.equal(staked.address)
+    })
+    it('should not show address when address does not have enough staked', async () => {
+      // Arrange
+      const [_, staked, staker] = await ethers.getSigners()
+      const { staking, token } = await loadFixture(deployAddressStakingV2)
+      const minStake = await staking.minStake()
+      const amount = minStake * 2n
+      await mintAndApprove(token, staker, staking, amount)
+      await staking.connect(staker).addStake(staked, amount / 2n)
+      await staking.connect(staker).addStake(staked, amount / 2n)
+      await staking.connect(staker).removeStake(0)
+      await staking.connect(staker).removeStake(1)
 
       // Act
       const addresses = await staking.stakedAddressesWithMinStake()
