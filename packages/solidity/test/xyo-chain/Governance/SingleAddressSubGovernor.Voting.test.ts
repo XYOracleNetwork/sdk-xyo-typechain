eb0b49f4a808254e918f706c90bac81721f3e737
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-09-06 09:00:04 -0500

    Short circuit voting tests

--- packages/solidity/test/xyo-chain/Governance/SingleAddressSubGovernor.Voting.test.ts
+++ packages/solidity/test/xyo-chain/Governance/SingleAddressSubGovernor.Voting.test.ts
@@ -2,15 +2,20 @@ import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers.js
 import { expect } from 'chai'
 import hre from 'hardhat'
 
-import { deploySingleAddressSubGovernor } from '../helpers/index.js'
+import {
+  createRandomProposal, deploySingleAddressSubGovernor, ProposalState, voteOnProposal,
+} from '../helpers/index.js'
 
 const { ethers } = hre
 
-describe('SingleAddressSubGovernor.castVote', () => {
-  it('should return correct vote weight for owner', async () => {
-    const { subGovernor, deployer } = await loadFixture(deploySingleAddressSubGovernor)
-
-    expect(await subGovernor.getVotes(deployer.address, 0)).to.equal(1)
-    expect(await subGovernor.getVotes(ethers.ZeroAddress, 0)).to.equal(0)
+describe.only('SingleAddressSubGovernor.castVote', () => {
+  describe('should not require voting delay when all votes cast with status', () => {
+    it('for', async () => {
+      const { subGovernor, deployer } = await loadFixture(deploySingleAddressSubGovernor)
+      const { proposalId } = await createRandomProposal(subGovernor)
+      await voteOnProposal(subGovernor, proposalId, deployer, 'For')
+      const finalState = await subGovernor.state(proposalId)
+      expect(finalState).to.equal(ProposalState.Succeeded)
+    })
   })
 })
