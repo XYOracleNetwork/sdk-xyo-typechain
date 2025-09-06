cdd84f7ae5dfa3e0125d1037502c9bdfc90018cb
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-09-06 09:45:56 -0500

    Notes on waiting for voting delay

--- packages/solidity/test/xyo-chain/Governance/SingleAddressSubGovernor.Voting.test.ts
+++ packages/solidity/test/xyo-chain/Governance/SingleAddressSubGovernor.Voting.test.ts
@@ -1,16 +1,15 @@
 import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers.js'
 import { expect } from 'chai'
-import hre from 'hardhat'
 
 import {
   advanceBlocks, createRandomProposal, deploySingleAddressSubGovernor, validateRandomProposalSucceeded, voteOnProposal,
 } from '../helpers/index.js'
 
-const { ethers } = hre
-
 describe('SingleAddressSubGovernor.castVote', () => {
   it('should not require waiting for voting delay when all votes cast with status for', async () => {
     // Arrange
+    // NOTE: Set this to true to see test succeed when waiting entire voting delay
+    const waitFullVotingDelay = false
     const { subGovernor, deployer } = await loadFixture(deploySingleAddressSubGovernor)
     const ctx = await createRandomProposal(subGovernor)
     const {
@@ -21,8 +20,8 @@ describe('SingleAddressSubGovernor.castVote', () => {
     await voteOnProposal(subGovernor, proposalId, deployer, 'For')
     // Verify subGovernor has voted on proposal
     expect(await subGovernor.hasVoted(proposalId, deployer)).to.equal(true)
-    // Advance required number of blocks
-    await advanceBlocks(await subGovernor.votingPeriod() + 1n)
+    // // Advance required number of blocks
+    if (waitFullVotingDelay) await advanceBlocks(await subGovernor.votingPeriod() + 1n)
     // Execute the proposal
     await subGovernor.execute(targets, values, calldatas, descriptionHash)
 
