37851e6d91ebcaf9f218d47b5f1a6a8a2770aaaa
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-09-09 11:06:46 -0500

    Add additional verification output

--- packages/solidity/scripts/CheckDeployment.ts
+++ packages/solidity/scripts/CheckDeployment.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-statements */
 import { readFileSync } from 'node:fs'
 import path from 'node:path'
 
@@ -14,19 +15,29 @@ const parseBigIntLiteral = (value: string): bigint => {
 }
 
 async function main() {
+  // Get network info
   const networkName = hre.network.name
   const chainId = hre.network.config.chainId
   console.log(`Running on: ${networkName} (Chain ID: ${chainId})`)
 
+  // Get deployment info
   const deploymentFile = path.join(__dirname, `../ignition/deployments/chain-${chainId}/deployed_addresses.json`)
   const deployments = JSON.parse(readFileSync(deploymentFile, 'utf8'))
 
+  // Get deployment config
   const deploymentConfigFile = path.join(__dirname, `../ignition/params.${networkName}.json`)
   const deploymentConfig = JSON.parse(readFileSync(deploymentConfigFile, 'utf8'))
-  const { bridgeTreasuryAddress, bridgeTreasuryAmount } = deploymentConfig.DeployXL1
 
+  // Get parameters for DeployXL1
+  const {
+    bridgeTreasuryAddress, bridgeTreasuryAmount, subGovernorCustodyAddress,
+  } = deploymentConfig.DeployXL1
+
+  // Display signers
   const [signer] = await hre.ethers.getSigners()
   console.log('Signer Address:', signer.address)
+
+  // Display deployed contract addresses
   const xl1Governance = await hre.ethers.getContractAt('XL1Governance', deployments['XL1Governance#XL1Governance'], signer)
   const xl1GovernanceAddress = await xl1Governance.getAddress()
   console.log('XL1 Governance Address:', xl1GovernanceAddress)
@@ -37,17 +48,30 @@ async function main() {
   const token = await hre.ethers.getContractAt('BridgeableToken', deployments['BridgeableToken#BridgeableToken'], signer)
   console.log('BridgeableToken Address:', await token.getAddress())
   const tokenOwner = await token.owner()
+
+  // Verify subGovernorCustodyAddress is the owner of the Single Address SubGovernor
+  const subGovernor = await hre.ethers.getContractAt('SingleAddressSubGovernor', deployments['SingleAddressSubGovernor#SingleAddressSubGovernor'], signer)
+  const subGovernorOwner = await subGovernor.owner()
+  console.log(expectedPrefix, 'SingleAddressSubGovernor Owner Address:', subGovernorCustodyAddress)
+  console.log(actualPrefix, 'SingleAddressSubGovernor Owner Address:', subGovernorOwner)
+
+  // Verify XL1 Governance is the owner of the BridgeableToken
   console.log(expectedPrefix, 'BridgeableToken Owner Address:', xl1GovernanceAddress)
   console.log(actualPrefix, 'BridgeableToken Owner Address:', tokenOwner)
+
+  // Verify expected treasury amount
   const balance = await token.balanceOf(bridgeTreasuryAddress)
   const decimals = await token.decimals()
   const normalizedTreasuryBalance = hre.ethers.formatUnits(balance, decimals)
   const normalizedExpectedTreasuryAmount = hre.ethers.formatUnits(parseBigIntLiteral(bridgeTreasuryAmount), decimals)
   console.log(expectedPrefix, 'BridgeableToken Treasury Balance:', normalizedExpectedTreasuryAmount)
   console.log(actualPrefix, 'BridgeableToken Treasury Balance:', normalizedTreasuryBalance)
+
+  // Verify expected total supply
   const totalSupply = await token.totalSupply()
   const normalizedTotalSupply = hre.ethers.formatUnits(totalSupply, decimals)
-  console.log('BridgeableToken Total Supply:', normalizedTotalSupply)
+  console.log(expectedPrefix, 'BridgeableToken Total Supply:', normalizedExpectedTreasuryAmount)
+  console.log(actualPrefix, 'BridgeableToken Total Supply:', normalizedTotalSupply)
 }
 
 main().catch(console.error)
