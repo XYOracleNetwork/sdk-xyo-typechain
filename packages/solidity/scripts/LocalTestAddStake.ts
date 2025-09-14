67d28477afb46f80fcc8504f83f1f8e17cc2992a
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-09-14 17:47:51 -0500

    Staked addresses

--- packages/solidity/scripts/LocalTestAddStake.ts
+++ packages/solidity/scripts/LocalTestAddStake.ts
@@ -13,16 +13,18 @@ const mintToStaker = async (staker: HardhatEthersSigner, amount: bigint, staking
   await erc20.mint(staker, amount)
 }
 
-const stakeAddress = async (staker: HardhatEthersSigner, amount: bigint, stakingTokenAddress: string, stakedXyoChainV2Address: string) => {
+const stakeAddress = async (staker: HardhatEthersSigner, staked: string, amount: bigint, stakingTokenAddress: string, stakedXyoChainV2Address: string) => {
   const stakedXyoChainV2 = await hre.ethers.getContractAt('StakedXyoChainV2', stakedXyoChainV2Address, staker)
   const stakeAmount = hre.ethers.parseUnits(amount.toString(), 18)
   await mintToStaker(staker, stakeAmount, stakingTokenAddress)
   const erc20 = await hre.ethers.getContractAt('BridgeableToken', stakingTokenAddress, staker)
   await erc20.approve(stakedXyoChainV2Address, stakeAmount)
-  await stakedXyoChainV2.addStake(staker, stakeAmount)
+  await stakedXyoChainV2.addStake(staked, stakeAmount)
+  const active = await stakedXyoChainV2.activeByAddressStaked(staked)
   const stakeResult = await stakedXyoChainV2.getStake(staker, 0)
   console.log('Stake Address:', staker)
   console.log('Stake Amount:', stakeAmount)
+  console.log('Active:', active)
   console.log('Stake:', stakeResult)
 }
 
@@ -44,21 +46,12 @@ async function main() {
   console.log('Staking Token Address:', stakingTokenAddress)
 
   // Stake producer
-  await stakeAddress(producerStaker, 10_000n, stakingTokenAddress, stakedXyoChainV2Address)
+  await stakeAddress(producerStaker, producerAddress, 10_000n, stakingTokenAddress, stakedXyoChainV2Address)
 
+  // Stake network address
   for (const [i, staker] of stakers.entries()) {
-    const stakedXyoChainV2 = await hre.ethers.getContractAt('StakedXyoChainV2', deployments['StakedXyoChainV2#StakedXyoChainV2'], staker)
-    const stakedXyoChainV2Address = await stakedXyoChainV2.getAddress()
     const stake = 100 * (i + 100)
-    const stakeAmount = hre.ethers.parseUnits(stake.toString(), 18)
-    await mintToStaker(staker, stakeAmount, stakingTokenAddress)
-    const erc20 = await hre.ethers.getContractAt('BridgeableToken', stakingTokenAddress, staker)
-    await erc20.approve(stakedXyoChainV2Address, stakeAmount)
-    await stakedXyoChainV2.addStake(staker, stakeAmount)
-    const stakeResult = await stakedXyoChainV2.getStake(staker, 0)
-    console.log('Stake Address:', staker)
-    console.log('Stake Amount:', stakeAmount)
-    console.log('Stake:', stakeResult)
+    await stakeAddress(staker, networkStakingAddress, BigInt(stake), stakingTokenAddress, stakedXyoChainV2Address)
   }
 }
 
