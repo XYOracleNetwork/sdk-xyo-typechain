93a0b828ecd2830b12acd5c0a23486f2bd3e668f
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-10-03 13:52:26 -0500

    Set payout address

--- packages/solidity/test/xyo-chain/helpers/deployLiquidityPoolBridge.ts
+++ packages/solidity/test/xyo-chain/helpers/deployLiquidityPoolBridge.ts
@@ -9,15 +9,15 @@ export const deployLiquidityPoolBridge = async (
   remoteChain?: string,
   maxBridgeAmount: bigint = ethers.parseUnits('1000000000', 18),
 ) => {
+  // Contracts are deployed using the first signer/account by default
+  const [owner] = await ethers.getSigners()
+
   // If no remote chain is provided, use a dummy address
   if (isUndefined(remoteChain)) remoteChain = getAddress('0x0000000000000000000000000000000000000001')
 
   // Deploy a LiquidityPoolBridge
-  const TokenFactory = await ethers.getContractFactory('LiquidityPoolBridge')
-  const bridge = await TokenFactory.deploy(remoteChain, token, maxBridgeAmount)
-
-  // Contracts are deployed using the first signer/account by default
-  const [owner] = await ethers.getSigners()
+  const LiquidityPoolBridgeFactory = await ethers.getContractFactory('LiquidityPoolBridge')
+  const bridge = await LiquidityPoolBridgeFactory.deploy(remoteChain, token, maxBridgeAmount, owner.address)
 
   return { bridge, owner }
 }
