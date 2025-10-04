e6d1e7bd5d3aa99ff057a6d159684b6552b23ed9
Joel Carter <JoelBCarter@users.noreply.github.com>
2025-10-04 10:18:48 -0500

    Bridge deploy scripts with params

--- packages/solidity/ignition/modules/LiquidityPoolBridge.ts
+++ packages/solidity/ignition/modules/LiquidityPoolBridge.ts
@@ -2,10 +2,12 @@ import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
 import type { IgnitionModuleBuilder } from '@nomicfoundation/ignition-core'
 
 const createLiquidityPoolBridgeModule = (m: IgnitionModuleBuilder) => {
-  const name = m.getParameter('tokenName')
-  const symbol = m.getParameter('tokenSymbol')
-  const token = m.contract('LiquidityPoolBridge', [name, symbol])
-  return { token }
+  const remoteChain = m.getParameter('remoteChain')
+  const token = m.getParameter('token')
+  const maxBridgeAmount = m.getParameter('maxBridgeAmount')
+  const payout = m.getParameter('payout')
+  const liquidityPoolBridge = m.contract('LiquidityPoolBridge', [remoteChain, token, maxBridgeAmount, payout])
+  return { liquidityPoolBridge }
 }
 
 export const LiquidityPoolBridgeModule = buildModule('LiquidityPoolBridge', createLiquidityPoolBridgeModule)
