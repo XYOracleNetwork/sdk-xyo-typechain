108e018d2dae56954a36894a1cbf581f459c0bc8
Matt Jones <885417+jonesmac@users.noreply.github.com>
2026-01-12 22:21:19 -0500

    deploy with liquidity source arg

--- packages/solidity/test/xyo-chain/helpers/deployLiquidityPoolBridge.ts
+++ packages/solidity/test/xyo-chain/helpers/deployLiquidityPoolBridge.ts
@@ -8,22 +8,22 @@ const DefaultMaxBridgeAmount = ethers.parseUnits('1000000000', 18)
 
 export const deployLiquidityPoolBridge = async (
   token: string,
-  payoutAddress?: string,
+  liquiditySource?: string,
   remoteChain?: string,
   maxBridgeAmount: bigint = DefaultMaxBridgeAmount,
 ) => {
   // Contracts are deployed using the first signer/account by default
-  const [owner] = await ethers.getSigners()
+  const [, _, hotWallet] = await ethers.getSigners()
 
   // If no remote chain is provided, use a dummy address
   if (isUndefined(remoteChain)) remoteChain = getAddress('0x0000000000000000000000000000000000000001')
 
-  // If no payout address is provided, use the owner's address
-  if (isUndefined(payoutAddress)) payoutAddress = owner.address
+  // If no liquidity source is provided, use the owner's address
+  if (isUndefined(liquiditySource)) liquiditySource = hotWallet.address
 
   // Deploy a LiquidityPoolBridge
   const LiquidityPoolBridgeFactory = await ethers.getContractFactory('LiquidityPoolBridge')
-  const bridge = await LiquidityPoolBridgeFactory.deploy(remoteChain, token, maxBridgeAmount, payoutAddress)
+  const bridge = await LiquidityPoolBridgeFactory.deploy(remoteChain, token, maxBridgeAmount, liquiditySource)
 
-  return { bridge, owner }
+  return { bridge }
 }
