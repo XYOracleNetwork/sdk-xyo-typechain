cba9b9f7c987a828742416dbcbfce8731a24d394
Arie Trouw <arie.trouw@xylabs.com>
2026-01-22 12:25:56 -0800

    sdk to packages folder and deploy/publish from xy tool

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
-export * as OpenZeppelin from '@xyo-network/open-zeppelin-typechain'
-export * as XYO from '@xyo-network/typechain'
-export * as Uniswap from '@xyo-network/uniswap-typechain'
-export * as World from '@xyo-network/world-typechain'
+// eslint-disable-next-line unicorn/require-module-specifiers
+export {}
