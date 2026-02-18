471f07c1af6e48f5e456935b105c2caaf25663c3
Joel Carter <JoelBCarter@users.noreply.github.com>
2026-02-18 08:42:55 -0600

    Documentation

--- packages/solidity/test/xyo-chain/helpers/deployLiquidityPoolBridge.ts
+++ packages/solidity/test/xyo-chain/helpers/deployLiquidityPoolBridge.ts
@@ -6,6 +6,15 @@ const { ethers } = hre
 
 const DefaultMaxBridgeAmount = ethers.parseUnits('1000000000', 18)
 
+/**
+ * Deploys a LiquidityPoolBridge contract with the specified parameters, providing defaults for optional parameters.
+ * @param token The address of the token to be bridged
+ * @param liquiditySource The address of the liquidity source for the bridge; if not provided, a default non-deployer account will be used
+ * @param remoteChain The address of the remote chain; if not provided, a dummy address will be used
+ * @param maxBridgeAmount The maximum amount that can be bridged; defaults to a large value
+ * @param owner The address of the owner; if provided, ownership will be transferred to this address
+ * @returns An object containing the deployed bridge contract
+ */
 export const deployLiquidityPoolBridge = async (
   token: string,
   liquiditySource?: string,
@@ -20,8 +29,8 @@ export const deployLiquidityPoolBridge = async (
   if (isUndefined(liquiditySource)) {
     // Use a non-deployer account as the liquidity source to avoid issues
     // with the bridge being the owner of the liquidity source
-    const [, _, hotWallet] = await ethers.getSigners()
-    liquiditySource = hotWallet.address
+    const [, _, defaultLiquiditySource] = await ethers.getSigners()
+    liquiditySource = defaultLiquiditySource.address
   }
 
   // Deploy a LiquidityPoolBridge
