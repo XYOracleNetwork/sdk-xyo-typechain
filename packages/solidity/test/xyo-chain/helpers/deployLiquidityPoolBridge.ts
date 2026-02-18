f62a88fe615099058309b338927974368609d07d
Joel Carter <JoelBCarter@users.noreply.github.com>
2026-02-18 08:34:36 -0600

    Conditionally use default liquidity source

--- packages/solidity/test/xyo-chain/helpers/deployLiquidityPoolBridge.ts
+++ packages/solidity/test/xyo-chain/helpers/deployLiquidityPoolBridge.ts
@@ -12,14 +12,16 @@ export const deployLiquidityPoolBridge = async (
   remoteChain?: string,
   maxBridgeAmount: bigint = DefaultMaxBridgeAmount,
 ) => {
-  // Contracts are deployed using the first signer/account by default
-  const [, _, hotWallet] = await ethers.getSigners()
-
   // If no remote chain is provided, use a dummy address
   if (isUndefined(remoteChain)) remoteChain = getAddress('0x0000000000000000000000000000000000000001')
 
-  // If no liquidity source is provided, use the owner's address
-  if (isUndefined(liquiditySource)) liquiditySource = hotWallet.address
+  // If no liquidity source is provided
+  if (isUndefined(liquiditySource)) {
+    // Use a non-deployer account as the liquidity source to avoid issues
+    // with the bridge being the owner of the liquidity source
+    const [, _, hotWallet] = await ethers.getSigners()
+    liquiditySource = hotWallet.address
+  }
 
   // Deploy a LiquidityPoolBridge
   const LiquidityPoolBridgeFactory = await ethers.getContractFactory('LiquidityPoolBridge')
